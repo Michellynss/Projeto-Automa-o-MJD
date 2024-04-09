a9ccb08d1570c927234a98b9253e0e5c797afb7a
Michellynss <112778162+Michellynss@users.noreply.github.com>
2024-04-09 17:37:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ port = 587
 email = os.environ['email']
 senha = os.environ['senha_email']
 remetente = os.environ['email']
-destinatarios = os.environ['destinatarios']       
+destinatarios = os.environ['destinatario1'], os.environ['destinatario2']         
 
 @app.route("/atualizacoes")
 def atualizacoes():
